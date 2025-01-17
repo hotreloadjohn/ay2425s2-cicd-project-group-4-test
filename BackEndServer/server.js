dc6963552641e3c30686d7bcc9a704df226f59e7
hotreloadjohn <johnng87@protonmail.com>
2025-01-17 22:45:09 +0800

    Refactor server.js for consistent code style and formatting

--- BackEndServer/server.js
+++ BackEndServer/server.js
@@ -4,14 +4,14 @@
 Summary: The server.js is used to start the backend server.
 */
 
-const path = require('path')
+const path = require("path");
 
-var express = require('express');
-var serveStatic = require('serve-static');
-var app = require('./controller/app.js');
+var express = require("express");
+var serveStatic = require("serve-static");
+var app = require("./controller/app.js");
 
 var port = 8081;
-app.use(express.static(path.join(__dirname, '../FrontEndServer/Public')))
-var server = app.listen(port, function(){
-    console.log('Web App Hosted at http://localhost:%s', port);
+app.use(express.static(path.join(__dirname, "../FrontEndServer/Public")));
+app.listen(port, function () {
+  console.log("Web App Hosted at http://localhost:%s", port);
 });
